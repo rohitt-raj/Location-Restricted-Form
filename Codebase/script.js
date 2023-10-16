c92bc99012c09e846a38c5f8d8582e11fc10071a
Rohit Raj <102911571+rohitt-raj@users.noreply.github.com>
2023-10-16 23:29:35 +0530

    Update script.js

--- Codebase/script.js
+++ Codebase/script.js
@@ -1,30 +1,30 @@
 navigator.geolocation.getCurrentPosition(posHandler, errHandler); 
 
+// when position fetched without errors
 function posHandler(position) {
   
-  const { latitude, longitude } = position.coords; 
-  //gets our current coordinates 
+  const { latitude, longitude } = position.coords; // fetches the latitude and longitude
+  
+  // displays the fetched location (a check for the API)
   homepage.innerHTML = "Your Latitude: " + latitude + "<br>"; 
   homepage.innerHTML += "Your Longitude: " + longitude + "<br>";
-  // the above two lines display our current location
+
+  // filter based on two coordinates system
   if(latitude>23.208942 && latitude<23.209558 && longitude>72.685342 && longitude<72.685868){
-    //the above checks if the location is in between specified coordinates
     homepage.innerHTML += "<h1>Access Granted: Form opening in 5 secs<h1>";
-    //5 second timeout given to check the location is correct or not
     setTimeout(()=>{
-      window.location.href = "Form/index.html"; // redirect to the form's location
+      window.location.href = "Form/index.html"; // redirect to the form's location in 5 secs
     }, 5000);
-    //implements the 5 second timeout
   }
   else{ 
     homepage.innerHTML += "<h1>Access Denied for the Form: Wrong Location :/ </h1>"; // custom error (not allowed) message
-    //display message for when the location is not favourable
   }
   
 };
 
+// in case any error occurs when using the Geolocation API
 function errHandler(error) {
-  console.error("Error ho rha:", error);
+  console.error("Error occured: ", error);
 }
 
 
@@ -33,11 +33,3 @@ function errHandler(error) {
  * 23.209558, 72.685342
  * 23.208942, 72.685868
  */
-
-
-// const watchId = navigator.geolocation.watchPosition(posHandler);
-
-// const optios = {
-//   timeout: 5000, // 5 seconds
-//   maximumAge: 0, // Force a fresh location
-// };
