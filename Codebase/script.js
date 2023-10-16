f541c4de1ad5dce91785f70405e0b4a60f54c5b9
Animesh Tumne <134190955+AnimeshTumne@users.noreply.github.com>
2023-10-16 23:35:46 +0530

    commented script.js aptly

--- Codebase/script.js
+++ Codebase/script.js
@@ -1,23 +1,32 @@
 navigator.geolocation.getCurrentPosition(posHandler, errHandler); 
+//this is responsible for prompting the user 
 
 // when position fetched without errors
 function posHandler(position) {
   
   const { latitude, longitude } = position.coords; // fetches the latitude and longitude
-  
-  // displays the fetched location (a check for the API)
+
   homepage.innerHTML = "Your Latitude: " + latitude + "<br>"; 
   homepage.innerHTML += "Your Longitude: " + longitude + "<br>";
+  // displays the fetched location (a check for the API)
 
-  // filter based on two coordinates system
+  // filter based on two coordinates system, flawed as the alignment of the room 
+  //may not always lie within the rectangles described by two coordintes (skewed geometry)
+  //needs revision to implement the correct function, described in the midterm project progress report 
   if(latitude>23.208942 && latitude<23.209558 && longitude>72.685342 && longitude<72.685868){
+    //temporary alignment via kyzeel hostel coordinates
     homepage.innerHTML += "<h1>Access Granted: Form opening in 5 secs<h1>";
     setTimeout(()=>{
       window.location.href = "Form/index.html"; // redirect to the form's location in 5 secs
+      // the redirected homepage resides in another folder, namely under the "Forms" folder in the same parent directory
+      
     }, 5000);
+    //the above waits for five seconds to redirect, as we had to check if our coordinates are displayed correctly or not
   }
+    
   else{ 
-    homepage.innerHTML += "<h1>Access Denied for the Form: Wrong Location :/ </h1>"; // custom error (not allowed) message
+    homepage.innerHTML += "<h1>Access Denied for the Form: Wrong Location :/ </h1>"; 
+    // custom error (not allowed) message
   }
   
 };
