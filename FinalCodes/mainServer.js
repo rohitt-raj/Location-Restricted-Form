53a8dcca437d77a3357165f1d2b04c4003552a4a
Rohit Raj <102911571+rohitt-raj@users.noreply.github.com>
2023-11-14 23:36:31 +0530

    Update mainServer.js

--- FinalCodes/mainServer.js
+++ FinalCodes/mainServer.js
@@ -2,11 +2,9 @@
 
 const fs = require('fs').promises;
 const path = require('path');
-const process = require('process');
 const {authenticate} = require('@google-cloud/local-auth');
 const {google} = require('googleapis');
 const express = require('express');
-const axios = require('axios');
 const app = express();
 const url = require('url');
 
