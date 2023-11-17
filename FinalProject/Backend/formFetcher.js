e2744644298b567b8aa0f9e62c2ddaf2572721ce
Rohit Raj <102911571+rohitt-raj@users.noreply.github.com>
2023-11-17 19:08:20 +0530

    Update formFetcher.js

--- FinalProject/Backend/formFetcher.js
+++ FinalProject/Backend/formFetcher.js
@@ -1,50 +1,52 @@
-const path = require('path');
-const google = require('@googleapis/forms');
-const {authenticate} = require('@google-cloud/local-auth');
-const express = require('express');
-const fs = require('fs');
+const path = require("path");
+const google = require("@googleapis/forms");
+const { authenticate } = require("@google-cloud/local-auth");
+const express = require("express");
+const fs = require("fs");
 const app = express();
 
 const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-async function runSample(formID) {
-    const auth = await authenticate({
-      keyfilePath: path.join(__dirname, 'credentials.json'),
-      scopes: 'https://www.googleapis.com/auth/forms.body.readonly',
-    });
-    const forms = google.forms({
-      version: 'v1',
-      auth: auth,
-    });
-    const res = await forms.forms.get({formId: formID});
-    return res.data;
-  }
+async function fetchData(formID) {
+  const auth = await authenticate({
+    keyfilePath: path.join(__dirname, "credentials.json"),
+    scopes: "https://www.googleapis.com/auth/forms.body.readonly",
+  });
+  const forms = google.forms({
+    version: "v1",
+    auth: auth,
+  });
+  const res = await forms.forms.get({ formId: formID });
+  return res.data;
+}
 
 // ===============================
-app.get('/getForm', (req, res) => {
-    async function getFormData(formID){
-        formData = await runSample(formID);
-        console.log(formData);
-        fs.writeFile("formData.json", JSON.stringify(formData), err => {
-            if(err) return console.log(err);
+app.get("/getForm", (req, res) => {
+    async function getFormData(formID) {
+        formData = await fetchData(formID);
+        fs.writeFile("for-student/formData.json", JSON.stringify(formData), (err) => {
+            if (err) return console.log(err);
             console.log("The file was saved!");
-        }); 
+            // res.status(200).json({
+            //     url: "10.7.11.14:5501/for-student/homepage.html",
+            // });
+        });
     }
-    
-    getFormData(req.query.formId);
 
-    res.status(200).send("DONE");
+    getFormData(req.query.formId);
 });
+
+// ============================= REDIRECT URI
+// app.get('/auth/google/callback', (req, res) => {
+//     res.json({
+//         url: "10.7.11.14:5501/for-student/homepage.html",
+//     });
+// });
 
 // =================
 const port = 3001;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
-
-
-
-
