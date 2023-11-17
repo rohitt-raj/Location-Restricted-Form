e0459eccfea253837de3c452a4bcf3347798039f
Rohit Raj <102911571+rohitt-raj@users.noreply.github.com>
2023-11-17 19:10:27 +0530

    Update locationFilter.js

--- FinalProject/Backend/for-student/locationFilter.js
+++ FinalProject/Backend/for-student/locationFilter.js
@@ -28,11 +28,11 @@ function twoPointDistance(coord1, coord2){
 
     let currAns = Math.sin(distanceLatitude/2)*Math.sin(distanceLatitude/2) + 
                   + Math.cos(nCoord1[0])*Math.cos(nCoord2[0]) 
-                  + Math.sin(distanceLongitude/2)*Math.sin(distanceLongitude/2);
+                  * Math.sin(distanceLongitude/2) * Math.sin(distanceLongitude/2);
     //the above derives from a formula to get the correct distance between any two points on the earths surface.
     currAns = 2 * Math.asin(Math.sqrt(currAns));
     let radiusOfEarth = 6371; //in metres
-    return currAns*currAns;
+    return currAns*radiusOfEarth;
     //the final distance in metres is returned
 }
 
@@ -53,29 +53,29 @@ function triangle(coordA, coordB, coordC){
 function isInside(roomCoord1, roomCoord2, roomCoord3, roomCoord4, myCoords){
     //each roomCoord and myCoord is a tuple [xCoord, yCoord]
 
-    //example roomCoords, of block seven
-    // let A = [23.213371, 72.684054]; 
-    // let B = [23.212907, 72.684584];
-    // let C = [23.213565, 72.685307];
-    // let D = [23.214002, 72.684747];
+    let [A, B, C, cosA, cosB, cosC] = triangle(roomCoord1, roomCoord2, myCoords);
 
-    //let A = []
-    //
-    // 
-    //
-
-    let A, B, C, cosA, cosB, cosC = triangle(roomCoord1, roomCoord2, myCoords);
     let AM_AB = C*B*cosA;
     let AB = C*C;
-    A, B, C, cosA, cosB, cosC = triangle(roomCoord1, myCoords, roomCoord3);
+
+    [A, B, C, cosA, cosB, cosC] = triangle(roomCoord1, myCoords, roomCoord4);
+
     let AM_AD = C*B*cosA;
     let AD = B*B;
+
+    console.log("Value of AM_AD", AM_AD);
+
     if(AM_AB>0 && AM_AB<AB){
         if(AM_AD>0 && AM_AD<AD){
             return 1;
         }
+        else{
+            return 0;
+        }
     }
-    return 0;
+    else{
+        return 0;
+    }
 }
 
 // =============================== HANDLES THE FETCHED LOCATION OF STUDENT
@@ -83,7 +83,10 @@ navigator.geolocation.getCurrentPosition(posHandler, errHandler);
 // when position fetched without errors
 function posHandler(position) {
     const { latitude, longitude } = position.coords; // fetches the latitude and longitude
-    myCoords = [latitude, longitude];
+    let myCoords = [latitude, longitude];
+
+    homepage.innerHTML = "Your Latitude: " + latitude + "<br>"; 
+    homepage.innerHTML += "Your Longitude: " + longitude + "<br>";
 
     // example roomCoords, of block seven
     // let roomCoord1 = [23.213371, 72.684054]; 
@@ -97,10 +100,12 @@ function posHandler(position) {
     let roomCoord3 = [23.208751, 72.685935];
     let roomCoord4 = [23.209621, 72.685908];
 
+    // location filtering
     if(isInside(roomCoord1, roomCoord2, roomCoord3, roomCoord4, myCoords)){
-        // window.location.href = "formInterface.html"; // redirect to the form's location
-        window.location.href = "formData.json"; // redirect to the form's location
-
+        homepage.innerHTML += "<h1>Access Granted: Form opening in 5 secs<h1>";
+        setTimeout(()=>{
+          window.location.href = "formInterface.html"; // redirect to the form's location in 5 secs
+        }, 5000);
     }
     else{ 
         homepage.innerHTML += "<h1>Access Denied for the Form: Wrong Location :/ </h1>"; // custom error (not allowed) message
