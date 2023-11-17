0390b52a97b936480935ac4be951ee5f0f468846
Animesh <animesh.tumne@iitgn.ac.in>
2023-11-17 19:40:55 +0530

    Added frontend changes

--- FinalProject/FrontEnd/script.js
+++ FinalProject/FrontEnd/script.js
@@ -112,76 +112,159 @@
 //   };
 
 // Define the variable to hold the quiz data
-let quizData = {}; // Initialize as empty object
+// let quizData = {}; // Initialize as empty object
 
-// Function to fetch custom JSON data
-function loadCustomJSON() {
-  fetch('./example.json')
-    .then(response => response.json())
-    .then(data => {
-      // Assign the fetched JSON data to the quizData variable
-      quizData = data;
-      console.log(quizData); // Display the data for demonstration
-      generateQuizForm();
-    })
-    .catch(error => console.error('Error fetching JSON:', error));
-}
-// Call the function to load the custom JSON data
-loadCustomJSON();
+// // Function to fetch custom JSON data
+// function loadCustomJSON() {
+//   fetch('./example.json')
+//     .then(response => response.json())
+//     .then(data => {
+//       // Assign the fetched JSON data to the quizData variable
+//       quizData = data;
+//       console.log(quizData); // Display the data for demonstration
+//       generateQuizForm();
+//     })
+//     .catch(error => console.error('Error fetching JSON:', error));
+// }
+// // Call the function to load the custom JSON data
+// loadCustomJSON();
 
 
-// Function to generate the quiz form
-function generateQuizForm() {
-    const formContainer = document.getElementById('formContainer');
-    quizData.items.forEach(item => {
-      const questionContainer = document.createElement('div');
+// // Function to generate the quiz form
+// function generateQuizForm() {
+//     const formContainer = document.getElementById('formContainer');
+//     quizData.items.forEach(item => {
+//       const questionContainer = document.createElement('div');
   
-      if (item.questionItem && item.questionItem.question) {
-        const questionElement = document.createElement('p');
-        questionElement.textContent = item.title;
-        questionContainer.appendChild(questionElement);
+//       if (item.questionItem && item.questionItem.question) {
+//         const questionElement = document.createElement('p');
+//         questionElement.textContent = item.title;
+//         questionContainer.appendChild(questionElement);
   
-        item.questionItem.question.choiceQuestion.options.forEach(option => {
-          const optionLabel = document.createElement('label');
-          const optionInput = document.createElement('input');
-          optionInput.type = 'radio';
-          optionInput.name = item.itemId;
-          optionInput.value = option.value;
+//         item.questionItem.question.choiceQuestion.options.forEach(option => {
+//           const optionLabel = document.createElement('label');
+//           const optionInput = document.createElement('input');
+//           optionInput.type = 'radio';
+//           optionInput.name = item.itemId;
+//           optionInput.value = option.value;
   
-          optionLabel.appendChild(optionInput);
-          optionLabel.appendChild(document.createTextNode(option.value));
-          questionContainer.appendChild(optionLabel);
-          questionContainer.appendChild(document.createElement('br'));
-        });
+//           optionLabel.appendChild(optionInput);
+//           optionLabel.appendChild(document.createTextNode(option.value));
+//           questionContainer.appendChild(optionLabel);
+//           questionContainer.appendChild(document.createElement('br'));
+//         });
   
-        formContainer.appendChild(questionContainer);
-      } else {
-        console.error(`Error: Question data missing for item with ID ${item.itemId}`);
-      }
-    });
+//         formContainer.appendChild(questionContainer);
+//       } else {
+//         console.error(`Error: Question data missing for item with ID ${item.itemId}`);
+//       }
+//     });
+//   }
+  
+  
+//   // Function to handle form submission
+//   function submitQuiz() {
+//     const answers = [];
+  
+//     quizData.items.forEach(item => {
+//       const selectedOption = document.querySelector(`input[name="${item.itemId}"]:checked`);
+//       if (selectedOption) {
+//         answers.push({ itemId: item.itemId, value: selectedOption.value });
+//       }
+//     });
+  
+//     // Here, you can handle the answers as needed (e.g., validate, process, send to server)
+//     console.log(answers);
+//   }
+  
+//   // Generate the quiz form when the page loads
+//   window.onload = function() {
+//     generateQuizForm();
+  
+//     const submitButton = document.getElementById('submitButton');
+//     submitButton.addEventListener('click', submitQuiz);
+//   };
+  
+const fs = require('fs');
+
+// Read the JSON file
+fs.readFile('../Backend/for-student/formData.json', 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error reading file:', err);
+    return;
   }
-  
-  
-  // Function to handle form submission
-  function submitQuiz() {
-    const answers = [];
-  
-    quizData.items.forEach(item => {
-      const selectedOption = document.querySelector(`input[name="${item.itemId}"]:checked`);
-      if (selectedOption) {
-        answers.push({ itemId: item.itemId, value: selectedOption.value });
+
+  try {
+    // Parse the JSON data
+    const jsonData = JSON.parse(data);
+
+    // Function to generate HTML for different question types...
+    function generateHTMLForQuestion(question) {
+      let html = '';
+      switch (question.type) {
+        case 'RADIO':
+          html += `<div>${question.title}</div>`;
+          question.options.forEach(option => {
+            html += `
+              <label>
+                <input type="radio" name="${question.questionId}" value="${option.value}">
+                ${option.value}
+              </label><br>
+            `;
+          });
+          break;
+        case 'CHECKBOX':
+          html += `<div>${question.title}</div>`;
+          question.options.forEach(option => {
+            html += `
+              <label>
+                <input type="checkbox" name="${question.questionId}" value="${option.value}">
+                ${option.value}
+              </label><br>
+            `;
+          });
+          break;
+        case 'DROP_DOWN':
+          html += `<div>${question.title}</div>`;
+          html += `<select name="${question.questionId}">`;
+          question.options.forEach(option => {
+            html += `<option value="${option.value}">${option.value}</option>`;
+          });
+          html += `</select>`;
+          break;
+        // Add more cases for other question types if needed
+        default:
+          html += `<div>${question.title}</div>`;
+          html += `<p>Question type not supported</p>`;
+          break;
       }
-    });
-  
-    // Here, you can handle the answers as needed (e.g., validate, process, send to server)
-    console.log(answers);
+      return html;
+    }
+
+    // Function to generate HTML from JSON...
+    function generateHTMLFromJSON(data) {
+      let html = '';
+      data.items.forEach(item => {
+        if (item.questionItem && item.questionItem.question) {
+          const question = item.questionItem.question;
+          if (question.choiceQuestion) {
+            html += generateHTMLForQuestion(question.choiceQuestion);
+          } else if (question.textQuestion) {
+            html += `<div>${question.title}</div>`;
+            html += `<textarea></textarea>`;
+          }
+          // Add more conditions for other question types if needed
+        }
+      });
+      return html;
+    }
+
+    // Get the container where you want to append the generated HTML
+    const container = document.getElementById('your-container-id'); // Replace with your container ID
+
+    // Generate HTML and append to the container
+    container.innerHTML = generateHTMLFromJSON(jsonData);
+  } catch (error) {
+    console.error('Error parsing JSON:', error);
   }
-  
-  // Generate the quiz form when the page loads
-  window.onload = function() {
-    generateQuizForm();
-  
-    const submitButton = document.getElementById('submitButton');
-    submitButton.addEventListener('click', submitQuiz);
-  };
-  
+});
